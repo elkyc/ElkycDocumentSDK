b642b2240e6b96bbe0333a97caf1cadf5d29d11e
Oleksii Hurin <dn070287gav@gmail.com>
2022-03-13 20:29:15 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             url: "https://pods.regulaforensics.com/DocumentReader/6.2.2422/DocumentReader-6.2.2422.zip",
             checksum: "0ff526f7aecbc45241dcf04e546ab6ce4067f4b6b4e19b31bf20d4b033e9c4d3"),
         .binaryTarget(
-            name: "DocumentReaderFullRFID",
+            name: "DocumentReaderCore",
             url: "https://pods.regulaforensics.com/FullRFID/6.2.5964/DocumentReaderCore_fullrfid_6.2.5964.zip",
             checksum: "cea2aae81daa80d1171cc7ecc2d0dea486ed2c13b58ef7bb893a44ca87e59014")
     ]
